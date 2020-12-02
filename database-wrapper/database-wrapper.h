86f977d86be20b4f422f1ec7098bba62ca7eb231
Murad <muradkarammaev@gmail.com>
2020-12-02 13:09:59 +0300

    add half functional authorization widget

--- database-wrapper/database-wrapper.h
+++ database-wrapper/database-wrapper.h
@@ -2,6 +2,12 @@
 #include <sqlite3.h>
 #include <stdbool.h>
 
+typedef enum {
+    DATABASE_USER,
+    DATABASE_ADMIN,
+    DATABASE_UNAUTHORIZED,
+} db_user_type;
+
 typedef struct database {
     sqlite3* db_file;
 } db;
@@ -11,3 +17,4 @@ db* database_open(char const* path, char** err);
 void database_close(db* database);
 
 bool database_register(db* database, char const* username, char const* password, bool is_admin, char** err);
+db_user_type database_check_credentials(db* database, char const* username, char const* password, char** err);
--- uni-books.cpp
+++ uni-books.cpp
@@ -1,20 +1,65 @@
-#include <Fl/Fl_Widget.H>
-#include <Fl/Fl_Window.H>
-#include <Fl/Fl_Menu_Bar.H>
-#include <Fl/Fl_Native_File_Chooser.H>
-#include <Fl/Fl_Double_Window.H>
+#include <FL/Fl.H>
+#include <FL/Fl_Widget.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Menu_Bar.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Native_File_Chooser.H>
+#include <FL/Fl_Double_Window.H>
 extern "C" {
 #   include "database-wrapper.h"
 }
 
-const unsigned WINDOW_SIZE_W = 800;
-const unsigned WINDOW_SIZE_H = 600;
+struct MainWindow;
+struct LoginWidget;
 
 static void menu_file_new_cb(Fl_Widget*, void*);
 static void menu_file_open_cb(Fl_Widget*, void*);
 static void menu_file_close_cb(Fl_Widget*, void*);
 static void menu_file_quit_cb(Fl_Widget*, void*);
 static void menu_help_about_cb(Fl_Widget*, void*);
+static void authorize_cb(Fl_Widget*, void*);
+
+struct LoginWidget: Fl_Widget {
+    LoginWidget(int x, int y, MainWindow* m): Fl_Widget(x, y, 400, 200) {
+        main_window = m;
+        box = new Fl_Box(x, y, 400, 200);
+        box->box(FL_UP_BOX);
+        label = new Fl_Box(x+20, y+20, 360, 20, "Log in");
+        username_input = new Fl_Input(x+80, y+40, 300, 40, "Username");
+        password_input = new Fl_Input(x+80, y+100, 300, 40, "Password");
+        authorize_button = new Fl_Button(x+150, y+150, 100, 40, "Authorize");
+        authorize_button->callback(authorize_cb, this);
+    }
+    ~LoginWidget() override {
+        delete box;
+        delete label;
+        delete username_input;
+        delete password_input;
+        delete authorize_button;
+    }
+    void draw() override {}
+    void hide() override {
+        box->hide();
+        label->hide();
+        username_input->hide();
+        password_input->hide();
+        authorize_button->hide();
+    }
+    void show() override {
+        box->show();
+        label->show();
+        username_input->show();
+        password_input->show();
+        authorize_button->show();
+    }
+    MainWindow* main_window;
+    Fl_Box* box;
+    Fl_Box* label;
+    Fl_Input* username_input;
+    Fl_Input* password_input;
+    Fl_Button* authorize_button;
+};
 
 struct MainWindow: Fl_Double_Window {
     MainWindow(int w, int h, char const* t): Fl_Double_Window(w, h, t) {
@@ -25,12 +70,16 @@ struct MainWindow: Fl_Double_Window {
         menu_bar->add("&File/&Close", "^w", menu_file_close_cb, this);
         menu_bar->add("&File/&Quit", "^q", menu_file_quit_cb, this);
         menu_bar->add("&Help/&About", "^/", menu_help_about_cb, this);
+        login_widget = new LoginWidget(200, 200, this);
+        login_widget->hide();
     }
     ~MainWindow() override {
         delete menu_bar;
+        delete login_widget;
     }
     db* database_handle;
     Fl_Menu_Bar* menu_bar;
+    LoginWidget* login_widget;
 };
 
 static void menu_file_new_cb(Fl_Widget*, void* m) {
@@ -65,11 +114,9 @@ ask_for_password:
     }
 }
 
-static void menu_file_open_cb(Fl_Widget*, void* m) {
+static void menu_file_open_cb(Fl_Widget* w, void* m) {
     auto* main_window = reinterpret_cast<MainWindow*>(m);
-    if (main_window->database_handle != nullptr) {
-        database_close(main_window->database_handle);
-    }
+    menu_file_close_cb(w, m);
     Fl_Native_File_Chooser file_chooser;
     file_chooser.type(Fl_Native_File_Chooser::BROWSE_FILE);
     if (file_chooser.show() != 0) {
@@ -82,13 +129,16 @@ static void menu_file_open_cb(Fl_Widget*, void* m) {
         fl_message("Failed to open database: %s", err);
         free(err);
     }
+    main_window->login_widget->show();
 }
 
 static void menu_file_close_cb(Fl_Widget*, void* m) {
     auto* main_window = reinterpret_cast<MainWindow*>(m);
     if (main_window->database_handle != nullptr) {
         database_close(main_window->database_handle);
+        main_window->database_handle = nullptr;
     }
+    main_window->login_widget->hide();
 }
 
 static void menu_file_quit_cb(Fl_Widget* w, void* m) {
@@ -100,8 +150,25 @@ static void menu_help_about_cb(Fl_Widget*, void*) {
     fl_message("There is no help");
 }
 
+static void authorize_cb(Fl_Widget*, void* l) {
+    auto login_widget = reinterpret_cast<LoginWidget*>(l);
+    char const* username = login_widget->username_input->value();
+    char const* password = login_widget->password_input->value();
+    if (username != nullptr && password != nullptr) {
+        char* err = nullptr;
+        db_user_type type = database_check_credentials(
+                login_widget->main_window->database_handle, username, password, &err);
+        if (type == DATABASE_UNAUTHORIZED) {
+            fl_message("Failed to authorize: %s", err);
+            free(err);
+        } else {
+            fl_message("Successfully authorized as %s", type == DATABASE_ADMIN ? "admin" : "casual user");
+        }
+    }
+}
+
 int main() {
-    MainWindow main_window(WINDOW_SIZE_W, WINDOW_SIZE_H, "Library");
+    MainWindow main_window(800, 600, "Library");
     main_window.end();
     main_window.show();
     return Fl::run();
