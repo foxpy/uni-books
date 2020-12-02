5f542ce98435d80fe6802fa8ebffed85ac5546cf
Murad <muradkarammaev@gmail.com>
2020-12-02 13:14:57 +0300

    lighter parameters for argon2
    Speeds up execution and tests.
    Safety does not matter after all, this is
    just a homework, lol.

--- database-wrapper/database-wrapper_impl/hash_password.c
+++ database-wrapper/database-wrapper_impl/hash_password.c
@@ -3,8 +3,8 @@
 #include <sodium/crypto_pwhash_argon2id.h>
 #include "database-wrapper_impl.h"
 
-#define ITERATIONS 4
-#define USED_MEMORY 1024 * 1024 * 2
+#define ITERATIONS 2
+#define USED_MEMORY 256 * 1024
 
 void hash_password(char const *pwd, char hash[static HASH_STR_SIZE]) {
     int rc = crypto_pwhash_argon2id_str(hash, pwd, strlen(pwd), ITERATIONS, USED_MEMORY);
