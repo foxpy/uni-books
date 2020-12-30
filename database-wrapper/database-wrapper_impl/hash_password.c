4bdd2e5ff2c9e473e7aec02d9a3cbead07167e5b
Murad <muradkarammaev@gmail.com>
2020-12-30 16:22:59 +0300

    fix warning in release build

--- database-wrapper/database-wrapper_impl/hash_password.c
+++ database-wrapper/database-wrapper_impl/hash_password.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <assert.h>
+#include <qc.h>
 #include <sodium/crypto_pwhash_argon2id.h>
 #include "database-wrapper_impl.h"
 
@@ -8,5 +9,8 @@
 
 void hash_password(char const *pwd, char hash[static HASH_STR_SIZE]) {
     int rc = crypto_pwhash_argon2id_str(hash, pwd, strlen(pwd), ITERATIONS, USED_MEMORY);
+#   ifdef NDEBUG
+        UNUSED(rc);
+#   endif
     assert(rc == 0);
 }
